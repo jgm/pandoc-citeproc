877323fc3bee6cc7a66369e1c7ab003ab209c9be
John MacFarlane <jgm@berkeley.edu>
2013-08-24 13:27:08 -0700

    Support for inline formatting.

--- src/Text/CSL/Pandoc.hs
+++ src/Text/CSL/Pandoc.hs
@@ -265,15 +265,39 @@ metaValueToJSON blockWriter _ (MetaBlocks bs) = liftM toJSON $ blockWriter bs
 metaValueToJSON _ inlineWriter (MetaInlines bs) = liftM toJSON $ inlineWriter bs
 
 blocksToString :: (Functor m, Monad m) => [Block] -> m String
-blocksToString [Plain xs] = inlinesToString xs
-blocksToString [Para xs] = inlinesToString xs
-blocksToString _ = fail "unsupported"
+blocksToString = fmap (unlines . intersperse "") . mapM go
+  where go (Plain xs) = inlinesToString xs
+        go (Para xs)  = inlinesToString xs
+        go _          = return ""
 
 inlinesToString :: (Functor m, Monad m) => [Inline] -> m String
 inlinesToString = fmap mconcat . mapM go
-  where go (Str xs) = return $ xs
-        go Space    = return " "
-        go _        = fail "unsupported"
+  where go (Str xs)         = return xs
+        go Space            = return " "
+        go (Emph xs)        = inTag "i" <$> inlinesToString xs
+        go (Strong xs)      = inTag "b" <$> inlinesToString xs
+        go (Superscript xs) = inTag "sup" <$> inlinesToString xs
+        go (Subscript xs)   = inTag "sub" <$> inlinesToString xs
+        go (SmallCaps xs)   = inTag "span style=\"font-variant:small-caps;\""
+                                   <$> inlinesToString xs
+        go (Code _ xs)      = return xs
+        go (Link xs _)      = inlinesToString xs
+        go (Image xs _)     = inlinesToString xs
+        go (RawInline f xs) | f == Format "citeproc"
+                            = return xs
+        go (Span _ xs)      = inlinesToString xs
+        go (Note _)         = return ""
+        go (LineBreak)      = return " "
+        go (Cite _ ils)     = inlinesToString ils
+        go (Quoted SingleQuote xs) = surround '‘' '’' <$> inlinesToString xs
+        go (Quoted DoubleQuote xs) = surround '“' '”' <$> inlinesToString xs
+        go _                = return ""
+
+surround :: Char -> Char -> String -> String
+surround beg end s = beg : s ++ [end]
+
+inTag :: String -> String -> String
+inTag t s = "<" ++ t ++ ">" ++ s ++ "</" ++ takeWhile (/=' ') t ++ ">"
 
 -- | Substitute 'Cite' elements with formatted citations.
 processCite :: Style -> M.Map [Citation] [FormattedOutput] -> Inline -> Inline
