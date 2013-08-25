0149e2fb7e2da75e2141b2236b9ba9b2009e8975
John MacFarlane <jgm@berkeley.edu>
2013-08-25 07:49:11 -0700

    Rename mods2yaml to biblio2yaml.
    The input file can now be any of the bibliographic formats
    bibutils supports.

--- biblio2yaml.hs
+++ biblio2yaml.hs
@@ -1,20 +1,18 @@
 module Main where
-import Text.CSL.Input.MODS (readModsCollection, readModsCollectionFile)
+import Text.CSL.Input.Bibutils (readBiblioFile)
 import Text.CSL.Pandoc ()
 import System.IO
 import System.Environment
 import System.Exit
 import Data.Yaml
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BL
 
 main :: IO ()
 main = do
   args <- getArgs
   refs <- case args of
-               [] -> readModsCollection `fmap` BL.getContents
                [x] | x /= "-h" && x /= "--help" ->
-                 readModsCollectionFile x
+                 readBiblioFile x
                _    -> do
                  hPutStrLn stderr $ "Usage: mods2yaml MODSFILE"
                  exitWith (ExitFailure 1)
