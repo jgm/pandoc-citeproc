404a8521ad440227d3c46c52558c12d0db698223
John MacFarlane <jgm@berkeley.edu>
2013-08-31 10:36:07 -0700

    Print warnings to stdout when bibliography items not found.

--- pandoc-citeproc.hs
+++ pandoc-citeproc.hs
@@ -1,7 +1,21 @@
 module Main where
 import Text.CSL.Pandoc (processCites')
 import Text.Pandoc.JSON
+import Text.Pandoc.Walk
+import System.IO (stderr, hPutStrLn)
 
 main :: IO ()
-main = toJSONFilter processCites'
+main = toJSONFilter go
+  where go :: Pandoc -> IO Pandoc
+        go doc = do
+           doc' <- processCites' doc
+           let warnings = query findWarnings doc'
+           mapM_ (hPutStrLn stderr) warnings
+           return doc'
 
+findWarnings :: Inline -> [String]
+findWarnings (Span (_,["citeproc-not-found"],[("data-reference-id",ref)]) _) =
+  ["pandoc-citeproc: reference " ++ ref ++ " not found"]
+findWarnings (Span (_,["citeproc-no-output"],_) _) =
+  ["pandoc-citeproc: reference with no printed form"]
+findWarnings _ = []
