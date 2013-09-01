91f50d61b13fbf40c9aeb5c2536124cf69bdcfbf
John MacFarlane <fiddlosopher@gmail.com>
2013-09-01 14:36:15 -0700

    Added --version, --help to pandoc-citeproc.

--- pandoc-citeproc.hs
+++ pandoc-citeproc.hs
@@ -3,15 +3,35 @@ import Text.CSL.Pandoc (processCites')
 import Text.Pandoc.JSON
 import Text.Pandoc.Walk
 import System.IO (stderr, hPutStrLn)
+import System.Console.GetOpt
+import System.Environment (getArgs)
+import Control.Monad
+import System.Exit
+import Data.Version (showVersion)
+import Paths_pandoc_citeproc (version)
 
 main :: IO ()
-main = toJSONFilter go
-  where go :: Pandoc -> IO Pandoc
-        go doc = do
-           doc' <- processCites' doc
-           let warnings = query findWarnings doc'
-           mapM_ (hPutStrLn stderr) warnings
-           return doc'
+main = do
+  argv <- getArgs
+  let (flags, _, errs) = getOpt Permute options argv
+  let header = "Usage: pandoc-citeproc"
+  unless (null errs) $ do
+    hPutStrLn stderr $ usageInfo (unlines $ errs ++ [header]) options
+    exitWith $ ExitFailure 1
+  when (Version `elem` flags) $ do
+    putStrLn $ "pandoc-citeproc " ++ showVersion version
+    exitWith ExitSuccess
+  when (Help `elem` flags) $ do
+    putStrLn $ usageInfo header options
+    exitWith ExitSuccess
+  toJSONFilter doCites
+
+doCites :: Pandoc -> IO Pandoc
+doCites doc = do
+  doc' <- processCites' doc
+  let warnings = query findWarnings doc'
+  mapM_ (hPutStrLn stderr) warnings
+  return doc'
 
 findWarnings :: Inline -> [String]
 findWarnings (Span (_,["citeproc-not-found"],[("data-reference-id",ref)]) _) =
@@ -19,3 +39,15 @@ findWarnings (Span (_,["citeproc-not-found"],[("data-reference-id",ref)]) _) =
 findWarnings (Span (_,["citeproc-no-output"],_) _) =
   ["pandoc-citeproc: reference with no printed form"]
 findWarnings _ = []
+
+data Option =
+    Help | Version
+  deriving (Ord, Eq, Show)
+
+options :: [OptDescr Option]
+options =
+  [ Option ['h'] ["help"] (NoArg Help) "show usage information"
+  , Option ['v'] ["version"] (NoArg Version) "show program version"
+  ]
+
+
