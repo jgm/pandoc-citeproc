2d9955b85e5c55b7a933f716fa57b271bf45e6de
John MacFarlane <fiddlosopher@gmail.com>
2013-09-12 23:07:44 -0700

    bibtex2pandoc:  Added bibtex types, parse non-dropping-particle.

--- bibtex2pandoc.hs
+++ bibtex2pandoc.hs
@@ -11,8 +11,9 @@ import Text.Parsec
 import Text.Pandoc
 import qualified Data.Map as M
 import Data.List.Split (splitOn)
+import Data.List (intersperse)
 import Data.Maybe
-import Data.Char (toLower)
+import Data.Char (toLower, isUpper)
 import System.Console.GetOpt
 import System.Environment
 import System.Exit
@@ -74,22 +75,41 @@ itemToMetaValue entry = MetaMap $ M.fromList fs'
                            "article"       -> "article-journal"
                            "book"          -> "book"
                            "booklet"       -> "pamphlet"
+                           "bookinbook"    -> "book"
+                           "collection"    -> "book"
+                           "electronic"    -> "webpage"
                            "inbook"        -> "chapter"
                            "incollection"  -> "chapter"
+                           "inreference "  -> "chapter"
                            "inproceedings" -> "paper-conference"
                            "manual"        -> "book"
                            "mastersthesis" -> "thesis"
                            "misc"          -> "no-type"
+                           "mvbook"        -> "book"
+                           "mvcollection"  -> "book"
+                           "mvproceedings" -> "book"
+                           "mvreference"   -> "book"
+                           "online"        -> "webpage"
+                           "patent"        -> "patent"
+                           "periodical"    -> "article-journal"
                            "phdthesis"     -> "thesis"
                            "proceedings"   -> "book"
+                           "reference"     -> "book"
+                           "report"        -> "report"
+                           "suppbook"      -> "chapter"
+                           "suppcollection" -> "chapter"
+                           "suppperiodical" -> "article-journal"
                            "techreport"    -> "report"
+                           "thesis"        -> "thesis"
                            "unpublished"   -> "manuscript"
+                           "www"           -> "webpage"
                            _               -> "no-type")
           ] ++ concat
           [ case entryType entry of
                  "phdthesis"     -> "genre" --> "Ph.D. thesis"
                  "mastersthesis" -> "genre" --> "Masters thesis"
                  _               -> []
+          , "type" ==> "genre"
           , "title" ==> "title"
           , "booktitle" ==> "container-title"
           , "series" ==> "collection-title"
@@ -107,6 +127,7 @@ itemToMetaValue entry = MetaMap $ M.fromList fs'
           , "address" ==> "publisher-place"
           , "author" *=> "author"
           , "editor" *=> "editor"
+          , "howpublished" ==> "note"
           , "issued" !=> concat
              [ "year" ==> "year"
              , "month" ==> "month"
@@ -121,22 +142,33 @@ toAuthorList x = error $ "toAuthorList: " ++ show x
 
 toAuthor :: [Inline] -> MetaValue
 toAuthor ils = MetaMap $ M.fromList
-  [ ("given", MetaList givens), ("family", family) ]
-  where endsWithComma (Str xs) = not (null xs) && last xs == ','
+  [ ("given", MetaList givens)
+  , ("family", family)
+  , ("non-dropping-particle", particle) ]
+  where endsWithComma (Str zs) = not (null zs) && last zs == ','
         endsWithComma _ = False
         stripComma xs = case reverse xs of
                              (',':ys) -> reverse ys
                              _ -> xs
         (xs, ys) = break endsWithComma ils
-        (family, givens) =
-           case ys of
-              (Str ws : rest) ->
-                  (MetaInlines $ xs ++ [Str (stripComma ws)], map MetaInlines
-                                                              $ splitOn [Space]
-                                                              $ dropWhile (== Space) rest)
-              _ -> case reverse $ splitOn [Space] xs of
-                        []     -> (MetaInlines [Str ""], [])
-                        (z:zs) -> (MetaInlines z, map MetaInlines $ reverse zs)
+        (family, givens, particle) =
+           case splitOn [Space] ys of
+              ((Str w:ws) : rest) ->
+                  ( MetaInlines [Str (stripComma w)]
+                  , map MetaInlines $ if null ws then rest else (ws : rest)
+                  , MetaInlines xs
+                  )
+              _ -> case reverse xs of
+                        []     -> (MetaInlines [], [], MetaInlines [])
+                        (z:zs) -> let (us,vs) = break startsWithCapital zs
+                                  in  ( MetaInlines [z]
+                                      , map MetaInlines $ splitOn [Space] $ reverse vs
+                                      , MetaInlines $ reverse us
+                                      )
+
+startsWithCapital :: Inline -> Bool
+startsWithCapital (Str (x:_)) = isUpper x
+startsWithCapital _           = False
 
 latex :: String -> MetaValue
 latex s = MetaBlocks bs
