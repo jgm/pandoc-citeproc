1b7e27279c45157bc3f67420ef5ed346562e5086
John MacFarlane <fiddlosopher@gmail.com>
2013-10-21 20:08:02 -0700

    Text.CSL.Output.Plain: Code cleanup for clarity and readibility.

--- src/Text/CSL/Output/Plain.hs
+++ src/Text/CSL/Output/Plain.hs
@@ -30,38 +30,40 @@ import Text.CSL.Style
 renderPlain :: [FormattedOutput] -> String
 renderPlain = concatMap $ render False
 
--- | Same as 'renderPlain' , but will not clean up the produced
--- output.
+-- | Same as 'renderPlain', but will not clean up the produced output.
 renderPlainStrict :: [FormattedOutput] -> String
 renderPlainStrict = concatMap $ render True
 
 render :: Bool -> FormattedOutput -> String
 render _ (FPan i) = show i
 render _ (FDel s) = s
-render b fo
-    | (FS str fm   ) <- fo = prefix fm <++> format fm (trim   str    ) <++> suffix fm
-    | (FErr _ )      <- fo = "???"
-    | (FN str fm   ) <- fo = prefix fm <++> format fm (trim   str    ) <++> suffix fm
-    | (FUrl t fm   ) <- fo = prefix fm <++> format fm (trim $ fst  t ) <++> suffix fm
-    | (FO     fm xs) <- fo = prefix fm <++> format fm (trim $ rest xs) <++> suffix fm
-    | otherwise            = []
+render noClean fo =
+  case fo of
+      (FS str fm   ) -> prefix fm <++> format fm (trim   str    ) <++> suffix fm
+      (FErr _ )      -> "???"
+      (FN str fm   ) -> prefix fm <++> format fm (trim   str    ) <++> suffix fm
+      (FUrl t fm   ) -> prefix fm <++> format fm (trim $ fst  t ) <++> suffix fm
+      (FO     fm xs) -> prefix fm <++> format fm (trim $ rest xs) <++> suffix fm
+      _              -> []
     where
-      rest  xs  = concatM (render b) xs
+      rest  xs  = concatM (render noClean) xs
 
-      trim      = if b then id   else unwords . words
-      (<++>)    = if b then (++) else (<>)
+      trim      = if noClean then id   else unwords . words
+      (<++>)    = if noClean then (++) else (<>)
       concatM f = foldr (<++>) [] . map f
 
-      quote  f s = if s /= [] && quotes f /= NoQuote then "\"" ++ s ++ "\"" else s
+      quote  f s = if null s || quotes f == NoQuote
+                      then s
+                      else "\"" ++ s ++ "\""
       format f s = quote f . text_case f $ s
 
-      text_case fm s
-          | "capitalize-first" <- textCase fm = capitalize s
-          | "capitalize-all"   <- textCase fm = unwords . map capitalize . words
-                                                $ s
-          | "lowercase"        <- textCase fm = map toLower s
-          | "uppercase"        <- textCase fm = map toUpper s
-          | otherwise = s
+      text_case fm s =
+        case textCase fm of
+            "capitalize-first" -> capitalize s
+            "capitalize-all"   -> unwords . map capitalize . words $ s
+            "lowercase"        -> map toLower s
+            "uppercase"        -> map toUpper s
+            _                  -> s
 
 (<+>) :: String -> String -> String
 [] <+> ss = ss
