b38d0f30aebece466fe811673153db3f06f67c7a
John MacFarlane <jgm@berkeley.edu>
2015-07-02 16:30:40 -0700

    Cleaned up Setup.hs.
    Now takes into account destdir in copying man page.

--- Setup.hs
+++ Setup.hs
@@ -2,43 +2,35 @@
 
 import Distribution.Simple
 import Distribution.Simple.PreProcess
-import Distribution.Simple.Setup
-         (copyDest, copyVerbosity, fromFlag, installVerbosity, BuildFlags(..))
+import Distribution.Simple.InstallDirs (mandir)
 import Distribution.PackageDescription (PackageDescription(..), Executable(..))
-import Distribution.Simple.LocalBuildInfo
-         (LocalBuildInfo(..), absoluteInstallDirs)
-import Distribution.Verbosity ( Verbosity, silent )
-import Distribution.Simple.InstallDirs (mandir, CopyDest (NoCopyDest))
-import Distribution.Simple.Utils (installOrdinaryFiles, info)
-import Prelude hiding (catch)
 import System.Process ( rawSystem )
 import System.FilePath ( (</>) )
 import System.Directory ( findExecutable )
-import System.Exit
+import Distribution.Simple.Utils (info, notice, rawSystemExit, installOrdinaryFiles)
+import Distribution.Simple.Setup
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Verbosity
+
 
 main :: IO ()
-main = do
+main =
   defaultMainWithHooks $ simpleUserHooks {
-      postCopy = \ _ flags pkg lbi ->
-         installManpages pkg lbi (fromFlag $ copyVerbosity flags)
-              (fromFlag $ copyDest flags)
-    , postInst = \ _ flags pkg lbi ->
-         installManpages pkg lbi (fromFlag $ installVerbosity flags) NoCopyDest
-    , hookedPreProcessors = [ppBlobSuffixHandler]
+      -- enable hsb2hs preprocessor for .hsb files
+      hookedPreProcessors = [ppBlobSuffixHandler]
+    , postCopy = installManPage
     }
-  exitWith ExitSuccess
 
-manpages :: [FilePath]
-manpages = ["man1" </> "pandoc-citeproc.1"]
-
-manDir :: FilePath
-manDir = "man"
-
-installManpages :: PackageDescription -> LocalBuildInfo
-                -> Verbosity -> CopyDest -> IO ()
-installManpages pkg lbi verbosity copy =
-  installOrdinaryFiles verbosity (mandir (absoluteInstallDirs pkg lbi copy))
-             (zip (repeat manDir) manpages)
+installManPage :: Args -> CopyFlags
+               -> PackageDescription -> LocalBuildInfo -> IO ()
+installManPage _ flags pkg lbi = do
+  let verbosity = fromFlag (copyVerbosity flags)
+  let copydest  = fromFlag (copyDest flags)
+  let mandest   = mandir (absoluteInstallDirs pkg lbi copydest)
+                     </> "man1"
+  notice verbosity $ "Copying man page to " ++ mandest
+  installOrdinaryFiles verbosity mandest
+     [("man" </> "man1", "pandoc-citeproc.1")]
 
 ppBlobSuffixHandler :: PPSuffixHandler
 ppBlobSuffixHandler = ("hsb", \_ _ ->
