33ad5e8e0a7743abd7b641d58f45ca1d8b309e12
John MacFarlane <jgm@berkeley.edu>
2017-06-04 14:25:47 +0200

    Use file-embed instead of hsb2hs for embed_data_files.
    Closes #285.

--- src/Text/CSL/Data/Embedded.hs
+++ src/Text/CSL/Data/Embedded.hs
@@ -1,16 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 -- to be processed using hsb2hs
 module Text.CSL.Data.Embedded (localeFiles, defaultCSL, manpage, license) where
+import Data.FileEmbed
 import qualified Data.ByteString.Char8 as S
 
 localeFiles :: [(FilePath, S.ByteString)]
-localeFiles = %blobs "locales/"
+localeFiles = $(embedDir "locales")
 
 defaultCSL :: S.ByteString
-defaultCSL = %blob "chicago-author-date.csl"
+defaultCSL = $(embedFile "chicago-author-date.csl")
 
 manpage :: S.ByteString
-manpage = %blob "man/man1/pandoc-citeproc.1"
+manpage = $(embedFile "man/man1/pandoc-citeproc.1")
 
 license :: S.ByteString
-license = %blob "LICENSE"
+license = $(embedFile "LICENSE")
