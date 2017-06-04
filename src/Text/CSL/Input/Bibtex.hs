0bc6f6bf894c071075ea7b13b8edc2d99992c438
John MacFarlane <jgm@berkeley.edu>
2017-06-04 14:07:13 +0200

    Allow + in bibtex field name.
    BibLaTeX now allows things like author+an for annotations.
    See "Data Annotations" in the bibtex manual
    and https://tex.stackexchange.com/questions/304644/formatting-inside-bib-files-with-biblatex
    
    Closes #290.
    
    Note that we don't actually parse the annotations, we just
    don't crash on bibtex files that include them.

--- src/Text/CSL/Input/Bibtex.hs
+++ src/Text/CSL/Input/Bibtex.hs
@@ -182,7 +182,7 @@ inQuotes = do
                       ) (char '"')
 
 fieldName :: BibParser String
-fieldName = (map toLower) <$> many1 (letter <|> digit <|> oneOf "-_:")
+fieldName = (map toLower) <$> many1 (letter <|> digit <|> oneOf "-_:+")
 
 isBibtexKeyChar :: Char -> Bool
 isBibtexKeyChar c = isAlphaNum c || c `elem` ".:;?!`'()/*@_+=-[]*"
