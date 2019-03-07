883a698a03b5f02f5da32ae1d6e894ed013d0b97
John MacFarlane <jgm@berkeley.edu>
2019-03-07 09:29:38 -0800

    Ensure that URIs are properly escaped.
    Closes #391.

--- src/Text/CSL/Eval.hs
+++ src/Text/CSL/Eval.hs
@@ -34,7 +34,7 @@ import           Data.Monoid            (Any (..))
 import           Data.String            (fromString)
 import qualified Data.Text              as T
 import           Text.Pandoc.Definition (Inline (Link, Span, Str), nullAttr)
-import           Text.Pandoc.Shared     (stringify)
+import           Text.Pandoc.Shared     (stringify, escapeURI)
 import           Text.Pandoc.Walk       (walk)
 
 import           Text.CSL.Eval.Common
@@ -239,7 +239,7 @@ evalElement el
                "page"        -> getStringVar "page" >>= formatRange fm
                "locator"     -> getLocVar >>= formatRange fm . snd
                "url"         -> getStringVar "url" >>= \k ->
-                                if null k then return [] else return [Output [OPan [Link nullAttr [Str k] (k,"")]] fm]
+                                if null k then return [] else return [Output [OPan [Link nullAttr [Str k] (escapeURI k,"")]] fm]
                "doi"         -> do d <- getStringVar "doi"
                                    let (prefixPart, linkPart) = T.breakOn (T.pack "http") (T.pack (prefix fm))
                                    let u = if T.null linkPart
@@ -247,20 +247,20 @@ evalElement el
                                               else T.unpack linkPart ++ d
                                    if null d
                                       then return []
-                                      else return [Output [OPan [Link nullAttr [Str (T.unpack linkPart ++ d)] (u, "")]]
+                                      else return [Output [OPan [Link nullAttr [Str (T.unpack linkPart ++ d)] (escapeURI u, "")]]
                                             fm{ prefix = T.unpack prefixPart, suffix = suffix fm }]
                "isbn"        -> getStringVar "isbn" >>= \d ->
                                 if null d
                                    then return []
-                                   else return [Output [OPan [Link nullAttr [Str d] ("https://worldcat.org/isbn/" ++ d, "")]] fm]
+                                   else return [Output [OPan [Link nullAttr [Str d] ("https://worldcat.org/isbn/" ++ escapeURI d, "")]] fm]
                "pmid"        -> getStringVar "pmid" >>= \d ->
                                 if null d
                                    then return []
-                                   else return [Output [OPan [Link nullAttr [Str d] ("https://www.ncbi.nlm.nih.gov/pubmed/" ++ d, "")]] fm]
+                                   else return [Output [OPan [Link nullAttr [Str d] ("https://www.ncbi.nlm.nih.gov/pubmed/" ++ escapeURI d, "")]] fm]
                "pmcid"       -> getStringVar "pmcid" >>= \d ->
                                 if null d
                                    then return []
-                                   else return [Output [OPan [Link nullAttr [Str d] ("https://www.ncbi.nlm.nih.gov/pmc/articles/" ++ d, "")]] fm]
+                                   else return [Output [OPan [Link nullAttr [Str d] ("https://www.ncbi.nlm.nih.gov/pmc/articles/" ++ escapeURI d, "")]] fm]
                _ -> do (opts, as) <- gets (env >>> options &&& abbrevs)
                        r <- getVar []
                               (getFormattedValue opts as f fm s) s
